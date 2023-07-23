2538d4e2c813af4db693b433fa23c38feb70072c
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-07-23 14:47:43 +0000

    Added run app

--- run_app.sh
+++ run_app.sh
@@ -2,4 +2,5 @@
 source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
 conda activate langchain_streamlit
 cd /home/ubuntu/projects/murli_chat
-/home/ubuntu/miniconda3/envs/langchain_streamlit/bin/streamlit run ./murli_chat.py --server.port 8080
+# /home/ubuntu/miniconda3/envs/langchain_streamlit/bin/streamlit run ./murli_chat.py --server.port 8080
+/home/ubuntu/miniconda3/envs/langchain_streamlit/bin/chainlit run ./murli_chat_chainlit.py --port 8080
